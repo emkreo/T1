4c1df6e5fb6466ce355e5763e310f3cf8fc710c9
Chester7777 <Es18.03.88@gmail.com>
2021-09-14 21:54:33 +0300

    Tooltip layout and placement in the header

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './App.css';
 
 import logo from './logo.svg';
 import { ChatPage } from './pages';
+import {ModeSwitch} from "./modeSwitch/modeSwitch";
 
 function App() {
   return (
     <div className='app'>
       <header className='app-header'>
         <img src={logo} className='app-logo' alt='logo' />
+        <ModeSwitch />
       </header>
       <div className='app-content'>
         <ChatPage />
--- src/modeSwitch/modeSwitch.tsx
+++ src/modeSwitch/modeSwitch.tsx
@@ -2,33 +2,21 @@ import * as React from 'react';
 
 import './modeSwitch.css';
 
-
-// const ReactHint = ReactHintFactory(React)
-class modeSwitch extends React.Component {
-  // onRenderContent = (target, content) => {
-  //   const {catId} = target.dataset
-  //   const width = 240
-  //   const url = `https://images.pexels.com/photos/${catId}/pexels-photo-${catId}.jpeg?w=${width}`
-  //
-  //   return <div className="custom-hint__content">
-  //     <img src={url} width={width} />
-  //     <button ref={(ref) => ref && ref.focus()}
-  //             onClick={() => this.instance.toggleHint()}>Ok</button>
-  //   </div>
-  // }
-
-  render() {
-    return <div className="mode-switch">
-      {/*<ReactHint autoPosition events delay= />*/}
-      {/*<ReactHint persist*/}
-      {/*           attribute="data-custom"*/}
-      {/*           className="custom-hint"*/}
-      {/*           events={{click: true}}*/}
-      {/*           onRenderContent={this.onRenderContent}*/}
-      {/*           ref={(ref) => this.instance = ref} />*/}
-
-      <button data-rh="Default">Default</button>
-      <button data-rh="Top" data-rh-at="top">Top</button>
-    </div>
+export class ModeSwitch extends React.Component {
+  render(): JSX.Element {
+    return (
+      <div className="container-mode-switch">
+        <div className="mode-switch">
+          <input className="size-text" type="radio" id="size-normal" name="color"
+                 value="indigo"/>
+          <label htmlFor="size-normal">14px</label>
+        </div>
+        <div className="mode-switch">
+          <input className="size-text" type="radio" id="size-large" name="color"
+                 value="red"/>
+          <label htmlFor="size-large">28px</label>
+        </div>
+      </div>
+    )
   }
 }
